201ee346f9b5e600d7824a74f3de2621e96d6ede
Andrew Nguyen Vo <awnguy01@louisville.edu>
2021-02-13 15:47:05 -0500

    Initial Spring project

---
+++ src/main/java/com/uol/ccc/cloudcostcalculatorservice/CloudCostCalculatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.uol.ccc.cloudcostcalculatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudCostCalculatorServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudCostCalculatorServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uol/ccc/cloudcostcalculatorservice/CloudCostCalculatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uol.ccc.cloudcostcalculatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudCostCalculatorServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
